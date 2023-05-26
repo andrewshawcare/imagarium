487b7f18cd005bbac4f7513d726fc2063f5cbf3f
Andrew Shaw Care <andrewshawcare@gmail.com>
2023-05-26 17:02:54 -0400

    Rename nodes so input panel is ordered correctly

---
+++ packages/imaginarium-client/nodes/2. JSON List Asset Selector.js
@@ -0,0 +1,113 @@
+const ImageElementCreator = require("image-element-creator");
+
+module.exports = async (node, graph) => {
+  const imageElementCreator = ImageElementCreator({
+    origin: configuration.serverOrigin
+  });
+
+  async function transitionStateAndRenderNode() {
+    await transitionState();
+    await renderNode();
+  }
+
+  async function transitionState() {
+    const itemListAssetPath = itemListAssetPathInput.value;
+
+    let itemList = [itemInput.default];
+    let defaultSelectedItem = itemInput.default;
+
+    if (itemListAssetPath !== itemListAssetPathInput.default) {
+      itemList = await (await fetch(itemListAssetPath)).json();
+
+      const itemListTitle = extractTitleFromAssetPath({
+        assetPath: itemListAssetPath
+      });
+      if (!itemList.includes(itemListTitle)) {
+        itemList.unshift(itemListTitle);
+      }
+      defaultSelectedItem = itemListTitle;
+    }
+
+    itemInput.options.values = itemList;
+
+    let item = itemInput.value;
+
+    if (!itemList.includes(item)) {
+      item = defaultSelectedItem;
+    }
+
+    itemInput.value = item;
+
+    itemOutput.setValue(
+      enabledInput.value && item !== defaultSelectedItem ? item : ""
+    );
+  }
+
+  async function renderNode() {
+    const item = itemInput.value;
+    const itemList = itemInput.options.values;
+
+    node.comment = item;
+
+    node.commentImage =
+      typeof item === "string" && item !== itemList[0]
+        ? await imageElementCreator.googleImagesSearchToImageElement({
+            query: item
+          })
+        : undefined;
+  }
+
+  function extractTitleFromAssetPath({ assetPath }) {
+    const titleRegularExpression = /(?<title>\w+)\..*$/;
+    const matches = assetPath.match(titleRegularExpression);
+    const title = matches.groups.title;
+    return `${title[0].toUpperCase()}${title.slice(1)}`;
+  }
+
+  const itemListAssetPathInput = node.in("Item List", "", {
+    type: "asset",
+    connectable: false
+  });
+  itemListAssetPathInput.onChange = async (
+    newItemListAssetPath,
+    oldItemListAssetPath
+  ) => {
+    if (
+      typeof newItemListAssetPath === "string" &&
+      typeof oldItemListAssetPath === "string" &&
+      newItemListAssetPath !== oldItemListAssetPath
+    ) {
+      await transitionStateAndRenderNode();
+    }
+  };
+
+  const itemInput = node.in("Item", "", {
+    type: "dropdown",
+    values: [""],
+    connectable: false
+  });
+  itemInput.onChange = async (newItem, oldItem) => {
+    if (
+      typeof newItem === "string" &&
+      typeof oldItem === "string" &&
+      newItem !== oldItem
+    ) {
+      await transitionStateAndRenderNode();
+    }
+  };
+
+  const itemOutput = node.out("Output", itemInput.value);
+
+  const enabledInput = node.in("Enabled", true, { connectable: false });
+  enabledInput.onChange = async (newEnabled, oldEnabled) => {
+    if (
+      typeof newEnabled === "boolean" &&
+      typeof oldEnabled === "boolean" &&
+      newEnabled !== oldEnabled
+    ) {
+      await transitionStateAndRenderNode();
+    }
+  };
+
+  node.onReady = transitionStateAndRenderNode;
+};
